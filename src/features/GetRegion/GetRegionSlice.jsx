c50ab45d36bd772fec4526574ed3e3a489c650f3
ashu00026 <19341a1207@gmrit.edu.in>
2023-05-03 22:58:16 +0530

    purge

--- src/features/GetRegion/GetRegionSlice.jsx
+++ src/features/GetRegion/GetRegionSlice.jsx
@@ -3,6 +3,7 @@ import React from "react";
 // import axios from "axios";
 import axios from "axios";
 import { active } from "d3";
+import { PURGE } from "redux-persist";
 
 const initialState = {
   isLoading: true,
